3a5faf1ba414a58f3ecbe75fc8b51a65d4894c78
TalonM <talonm@uw.edu>
2020-11-20 17:14:01 -0800

    modified the wordCount() method to take a String parameter and return the sorted map instead of just printing it

--- src/test/java/HandleTruthTest.java
+++ src/test/java/HandleTruthTest.java
@@ -1,8 +1,13 @@
 import org.junit.Test;
 
+import java.util.Set;
+import java.util.TreeMap;
+
 public class HandleTruthTest {
     @Test
     public void makeshiftTest() {
-        HandleTruth.wordCount();
+
+        TreeMap<Integer, Set<String>> map = HandleTruth.wordCount("bob eats corn on the cob for bob loves corn");
+
     }
 }
